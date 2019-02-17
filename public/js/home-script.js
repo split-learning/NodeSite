50aaa819eac2ceb83799de881a503ffd6bc58e10
Matthew Graham <mpgraham917@gmail.com>
2019-02-17 00:20:25 -0500

    svgs and home page

---
+++ public/js/home-script.js
@@ -0,0 +1,29 @@
+let row_js = [
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+]
+let row_csharp = [
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+]
+let row_frameworks = [
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+]
+let row_php = [
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+]
+let row_ruby = [
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+    { title: "", channel: "", img_src: "" },
+]
+
+$("#rows-container").ready(() => {
+    console.log('row container ready')
+})
