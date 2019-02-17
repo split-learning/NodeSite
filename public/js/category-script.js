bf9693f94cd2c7be30373797a78d91bbd733d2ff
Jesse Saran <8915311+swrap@users.noreply.github.com>
2019-02-17 03:32:43 -0500

    Updating category

--- public/js/category-script.js
+++ public/js/category-script.js
@@ -27,7 +27,7 @@ $("#category-rows-container").ready(() => {
 
         category.data.forEach(data => {
             let video_cell = $("<div class='video-cell'>")
-            video_cell.append($("<img class='video-thumbnail' src="+data.img_src+"/>"))
+            video_cell.append($("<img class='video-thumbnail' src=\""+data.img_src+"\"/>"))
             video_cell.append($("<div class='video-title'>").html(data.title))
             video_cell.append($("<div class='video-channel'>").html(data.channel))
             videos.append(video_cell)
@@ -35,4 +35,19 @@ $("#category-rows-container").ready(() => {
         row.append(videos)
         $("#category-rows-container").append(row)
     })
+});
+
+$(".banner-container").ready(() => {
+    let row = $("<div class='row'>")
+
+    let banner = $("<div class='banner' style='margin-top:10px;'>")
+
+    let banner_cell = $("<div class='banner-cell'>")
+    banner_cell.append($("<img class='banner-thumbnail' src=\"/assets/thumbnails/"+encodeURIComponent(cat)+"_logo.png\"/>"))
+    banner_cell.append($("<div class='banner-title'>").html("Welcome to the " + cat + " Channel"))
+    banner_cell.append($("<div class='banner-channel'>").html("Come try some " + cat + " tutorials!"))
+    banner.append(banner_cell)
+
+    row.append(banner)
+    $(".banner-container").append(row);
 });
