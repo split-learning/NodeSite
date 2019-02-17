bbb57b8aee4e68aa7795ff4c2d983154d5695df5
Matthew Graham <mpgraham917@gmail.com>
2019-02-17 01:00:42 -0500

    Just some stuff what you so nosey for?

--- public/js/home-script.js
+++ public/js/home-script.js
@@ -1,29 +1,50 @@
+let row_featured = [
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Frameworks/frame_3.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Ruby/ruby_2.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/javascript/javascript_2.jpg" },
+]
+let row_trending = [
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/javascript/javascript_1.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/C#/C#_1.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Frameworks/frame_1.jpg" },
+]
 let row_js = [
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/javascript/javascript_1.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/javascript/javascript_2.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/javascript/javascript_3.jpg" },
 ]
 let row_csharp = [
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/C#/C#_1.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/C#/C#_2.jpg" },
 ]
 let row_frameworks = [
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Frameworks/frame_1.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Frameworks/frame_2.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Frameworks/frame_3.jpg" },
 ]
 let row_php = [
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Php/php_1.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Php/php_2.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Php/php_3.jpg" },
 ]
 let row_ruby = [
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
-    { title: "", channel: "", img_src: "" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Ruby/ruby_1.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Ruby/ruby_2.jpg" },
+    { title: "A Video", channel: "Astronaut", img_src: "/assets/thumbnails/Ruby/ruby_3.jpg" },
 ]
 
+let rows = [row_featured, row_trending, row_js, row_csharp, row_frameworks, row_php, row_ruby]
+
 $("#rows-container").ready(() => {
-    console.log('row container ready')
+    rows.forEach(category => {
+        let row = $("<div class='row'>")
+        category.forEach(vid => {
+            let video_cell = $("<div class='video_cell'>")
+            video_cell.append($("<img class='video_thumbnail' src="+vid.img_src+"/>"))
+            video_cell.append($("<div class='video_title'>").html(vid.title))
+            video_cell.append($("<div class='video_channel'>").html(vid.channel))
+            row.append(video_cell)
+        })
+    })
+    $("#rows-container").append(rows)
 })
